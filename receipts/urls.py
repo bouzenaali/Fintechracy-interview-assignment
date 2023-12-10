2ea1d656b7c8a0d367bb8181aa5c18f71294628d
bouzenaali <Alibouzena16@gmail.com>
2023-12-10 21:28:55 +0100

    feat: edit the urls

---
+++ receipts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'receipts'
+
+urlpatterns = [
+    path('', views.ListReceipts, name='list'),
+    path('new/', views.new, name='new'),
+    path('<int:pk>/detail', views.DetailReceipt, name='detail'),
+    path('<int:pk>/delete', views.delete, name='delete'),
+    path('<int:pk>/edit', views.edit, name='edit'),
+    path('<int:pk>/detail', views.DetailReceipt, name='detail')
+]
