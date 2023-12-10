4714fdc865600244ebef72dbf24e693bf9b66777
bouzenaali <Alibouzena16@gmail.com>
2023-12-10 11:48:44 +0100

    feat(receipts): add the add and edit forms

--- receipts/models.py
+++ receipts/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class Receipt(models.Model):
     store_name = models.CharField(max_length=100)
-    date_of_purchase = models.DateTimeField()
+    date_of_purchase = models.DateField()
     item_list = models.TextField()
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='Receipts', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.store_name} - {self.date_of_purchase}"
