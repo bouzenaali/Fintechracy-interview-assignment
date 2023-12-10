1eccade13581f2d0b710a134a65c23a2229496a9
bouzenaali <Alibouzena16@gmail.com>
2023-12-10 11:33:02 +0100

    feat(receipts): add the receipt model

--- receipts/models.py
+++ receipts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Receipt(models.Model):
+    store_name = models.CharField(max_length=100)
+    date_of_purchase = models.DateTimeField()
+    item_list = models.TextField()
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.store_name} - {self.date_of_purchase}"
+
