704eb03ea5dc46072bafb5b8a8d4df39545d706f
bouzenaali <Alibouzena16@gmail.com>
2023-12-12 22:19:51 +0100

    feat(project): add authentication to the urls

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('receipts.urls')),
+    path('app/', include('receipts.urls')),
+    path('', include('authentication.urls')),
 ]
