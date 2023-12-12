009a98b868bf90a4e6a74c1ae530462ef1166ab4
bouzenaali <Alibouzena16@gmail.com>
2023-12-12 16:27:58 +0100

    feat(authentication): add the signup and logout views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import SignUpForm
 
-# Create your views here.
+def signup(request):
+    if (request.method == 'POST'):
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'authentication/signup.html', {
+        'form': form
+    })
+
+
+def logout(request):
+    logout(request)
+    return redirect('recipes:index')
