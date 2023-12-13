6e5a63d77a4597324a1908713f26b0c8d45ca00f
bouzenaali <Alibouzena16@gmail.com>
2023-12-13 19:36:00 +0100

    test: add some basic tests on the models

---
+++ receipts/tests/test_models.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from receipts.models import Receipt, User
+
+class ReceiptModelTest(TestCase):
+    def test_receipt_creation(self):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        receipt = Receipt.objects.create(
+            store_name='Test Store',
+            date_of_purchase='2023-12-15',
+            item_list='Item 1, Item 2',
+            total_amount=100.0,
+            created_by=user
+        )
+
+
+        self.assertEqual(receipt.store_name, 'Test Store')
+        self.assertEqual(receipt.date_of_purchase, '2023-12-15')
+        self.assertEqual(receipt.item_list, 'Item 1, Item 2')
+        self.assertEqual(receipt.total_amount, 100.0)
